0e8f327ea0ad9965dc6a9baae0b08687c9fd9f7a
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-09-02 20:05:04 +0300

    Do not mix pac-ret and ptrauth_returns

--- libunwind/src/DwarfInstructions.hpp
+++ libunwind/src/DwarfInstructions.hpp
@@ -303,6 +303,12 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace,
       isSignalFrame = cieInfo.isSignalFrame;
 
 #if defined(_LIBUNWIND_TARGET_AARCH64)
+      // There are two ways of return address signing: pac-ret (enabled via
+      // -mbranch-protection=pac-ret) and ptrauth-returns (enabled as part of
+      // Apple's arm64e or experimental pauthtest ABI on Linux). The code
+      // below handles signed RA for pac-ret, while ptrauth-returns uses
+      // different logic.
+#if !__has_feature(ptrauth_returns)
       // If the target is aarch64 then the return address may have been signed
       // using the v8.3 pointer authentication extensions. The original
       // return address needs to be authenticated before the return address is
@@ -343,6 +349,7 @@ int DwarfInstructions<A, R>::stepWithDwarf(A &addressSpace,
         returnAddress = x17;
 #endif
       }
+#endif
 #endif
 
 #if defined(_LIBUNWIND_IS_NATIVE_ONLY) && defined(_LIBUNWIND_TARGET_ARM) &&    \
